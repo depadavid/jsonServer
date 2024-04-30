c35e992fb527f031268e663819e29be6b29f5b59
depadavid <100303655+depadavid@users.noreply.github.com>
2024-04-30 08:16:56 -0500

    feat: :construction: Conulta 6: Obtención cliente de España
    Conulta 6: Devuelve un listado con el nombre de todo los clientes españoles

--- js/modules/clientes.js
+++ js/modules/clientes.js
@@ -14,3 +14,13 @@ async function getClientesPorCiudad(ciudad) {
     const clientes = await response.json()
     return clientes
 }
+
+// 6. Devuelve un listado con el nombre de los todos los clientes españoles.
+
+async function getClientesDeEspana() {
+    const clientes = await getClientesPorPais("Spain")
+    const nombresClientes = clientes.map(({client_name}) => ({client_name}))
+    console.log(nombresClientes);
+}
+
+//  getClientesFromSpain()
