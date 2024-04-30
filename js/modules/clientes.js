d38f7188a0c6e3d99db29db85c80bdde0968ada2
depadavid <100303655+depadavid@users.noreply.github.com>
2024-04-30 08:15:20 -0500

    feat: :construction: Obtencion cliente por ciudad
    Esta función devuelve lo clientes por cuidad

--- js/modules/clientes.js
+++ js/modules/clientes.js
@@ -3,3 +3,14 @@ async function getClientes() {
     const clientes = await response.json()
     return clientes
 }
+async function getClientesPorPais(pais) {
+    const response = await fetch(`http://localhost:3000/clients?country=${pais}`)
+    const clientes = await response.json()
+    return clientes
+}
+
+async function getClientesPorCiudad(ciudad) {
+    const response = await fetch(`http://localhost:3000/clients?country=${region}`)
+    const clientes = await response.json()
+    return clientes
+}
