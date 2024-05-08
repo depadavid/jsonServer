4e196c268176615d1317fe8b167f82d543417eb1
depadavid <100303655+depadavid@users.noreply.github.com>
2024-05-08 07:59:29 -0500

    feat: :construction: Funcion get empleado por Id 1
    Devuelve los empleados que tenga el Id número 1

--- js/modules/empleado.js
+++ js/modules/empleado.js
@@ -7,3 +7,9 @@ async function getEmpleadoPorCodigo(codigo) {
     const response = await fetch(`http://localhost:5501/employee?code_boss=${codigo}`)
     return await response.json()
 }
+export async function getEmpleadoPorId(id) {
+    const response = await fetch(`http://localhost:5501/employee?employee_code=${id}`)
+    return await response.json()
+}
+
+// console.log(getEmpleadoPorId(1));
